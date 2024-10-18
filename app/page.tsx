248e6f0385d6eab73e39f548e91c62a6f211e2af
sitammeur <sitammeur@gmail.com>
2024-10-18 21:01:41 +0000

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import CodeEditor from "./components/CodeEditor";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="code-editor-ref">
+        <CodeEditor />
+      </div>
+    </main>
+  );
+}
