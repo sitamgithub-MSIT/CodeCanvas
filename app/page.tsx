b2c1d3b755ccc54f46fe65531c62cacc84d31a13
sitammeur <sitammeur@gmail.com>
2024-10-19 07:01:20 +0000

    codebase re-structured

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import CodeEditor from "./components/CodeEditor";
+import CodeEditor from "../components/CodeEditor";
 
 export default function Home() {
   return (
---
+++ components/CodeEditor.tsx
@@ -0,0 +1,54 @@
+"use client";
+import React from "react";
+import AceEditor from "react-ace";
+import { Resizable } from "re-resizable";
+
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/mode-xml";
+import "ace-builds/src-noconflict/mode-ruby";
+import "ace-builds/src-noconflict/mode-sass";
+import "ace-builds/src-noconflict/mode-markdown";
+import "ace-builds/src-noconflict/mode-mysql";
+import "ace-builds/src-noconflict/mode-json";
+import "ace-builds/src-noconflict/mode-html";
+import "ace-builds/src-noconflict/mode-golang";
+import "ace-builds/src-noconflict/mode-csharp";
+import "ace-builds/src-noconflict/mode-golang";
+import "ace-builds/src-noconflict/mode-css";
+
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/theme-terminal";
+import "ace-builds/src-noconflict/theme-twilight";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/theme-tomorrow";
+import "ace-builds/src-noconflict/theme-kuroir";
+import "ace-builds/src-noconflict/theme-xcode";
+import "ace-builds/src-noconflict/theme-textmate";
+import "ace-builds/src-noconflict/theme-solarized_dark";
+import "ace-builds/src-noconflict/theme-solarized_light";
+
+function CodeEditor() {
+  return (
+    <Resizable minWidth={510} maxWidth={1000} minHeight={466}>
+      <div>
+        <AceEditor
+          value="function() {return 'Hello World';}"
+          name="UNIQUE_ID_OF_DIV"
+          fontSize={16}
+          theme="solarized_dark"
+          mode="javascript"
+          showGutter={false}
+          wrapEnabled={true}
+          showPrintMargin={false}
+          highlightActiveLine={false}
+          editorProps={{ $blockScrolling: true }}
+          className="ace-editor-container"
+        />
+      </div>
+    </Resizable>
+  );
+}
+
+export default CodeEditor;
