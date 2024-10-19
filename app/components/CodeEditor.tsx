e0310398e93c7ed1a135220773232539eb89d3cf
sitammeur <sitammeur@gmail.com>
2024-10-19 05:37:43 +0000

    codeeditor component update

--- app/components/CodeEditor.tsx
+++ app/components/CodeEditor.tsx
@@ -1,8 +1,26 @@
 "use client";
 import React from "react";
+import AceEditor from "react-ace";
+import { Resizable } from "re-resizable";
 
-const CodeEditor = () => {
-  return <div>CodeEditor</div>;
-};
+function CodeEditor() {
+  return (
+    <Resizable minWidth={510} maxWidth={1000} minHeight={466}>
+      <div>
+        <AceEditor
+          value="function() {return 'Hello World';}"
+          name="UNIQUE_ID_OF_DIV"
+          fontSize={16}
+          showGutter={false}
+          wrapEnabled={true}
+          showPrintMargin={false}
+          highlightActiveLine={false}
+          editorProps={{ $blockScrolling: true }}
+          className="ace-editor-container"
+        />
+      </div>
+    </Resizable>
+  );
+}
 
 export default CodeEditor;
